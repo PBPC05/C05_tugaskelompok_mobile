95a476b43184bf659b8888c5a2752cf5b2432646
icethetic <ammarmuhraf@gmail.com>
2025-11-24 16:03:55 +0700

    Feat: login, register integration within Django still error, need debug

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,35 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:pittalk_mobile/features/authentication/presentation/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      create: (_) {
+        CookieRequest request = CookieRequest();
+        return request;
+      },
+      child: MaterialApp(
+        title: 'PitTalk',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.red,
+          ).copyWith(
+            secondary: Colors.redAccent[400],
+          ),
+          useMaterial3: true,
+        ),
+        home: const LoginPage(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
 }
